543cc83c642cd989caadd5fc3d0794b706d5ccc2
SongKY <55125295+fairsky0915@users.noreply.github.com>
2024-07-04 12:32:05 +0900

    new js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,43 @@
 
 /// <reference types="@fastly/js-compute" />
 
+var ConsistentHash = require('consistent-hash');
+var hr = new ConsistentHash({distribution: "uniform"});
+hr.add('origin_0');
+hr.add('origin_1');
 
+const parseCookie = str => 
+  str
+  .spilt(';')
+  .map(v => v.spilt('='))
+  .reduce((acc, v) => {
+    add[decodeURIComponent(v[0].trim())] = decodeURIComponent(v[1].trim())
+    return acc;
+  }, {});
+
+addEventListener("fetch", event => {
+   //Get the request from the client
+  const req = event.request;
+
+  //The hash will use the use id to generate a hash key
+  const cookies = parseCookie(req.headers.get("cookie"));
+  const resourceName = cookies["user-id"];
+  const backendToUse = hr.get(resourceName);
+  console.log(resourceName + " --> " + backendToUse);
+//Send the request to whichever origin is picked by the hash.
+const backendResponse = fetch(req, {
+  backend:backendToUse
+});
+
+//Send the backend Response back to the client
+event.respondWith(backendResponse);
+
+});
+
+
+
+
+/*
 addEventListener("fetch", event => {
   // Get the request from the client.
   const req = event.request;
@@ -31,6 +67,7 @@ addEventListener("fetch", event => {
   // Send the backend response back to the client.
   event.respondWith(backendResponse);
 });
+*/
 
 /*
 import { Router } from "@fastly/expressly";
